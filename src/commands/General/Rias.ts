6ddfec82b71d69403829a4aa9bbb903098150f73
Issei <74949779+VaseemStarkJr3000@users.noreply.github.com>
2022-05-16 00:19:58 +0530

    Update Rias.ts

--- src/commands/General/Rias.ts
+++ src/commands/General/Rias.ts
@@ -1,34 +1,93 @@
 /** @format */
 
 import { MessageType, Mimetype } from "@adiwajshing/baileys";
+
 import MessageHandler from "../../Handlers/MessageHandler";
+
 import BaseCommand from "../../lib/BaseCommand";
+
 import WAClient from "../../lib/WAClient";
+
 import { ISimplifiedMessage } from "../../typings";
 
 export default class Command extends BaseCommand {
-	constructor(client: WAClient, handler: MessageHandler) {
-		super(client, handler, {
-			command: "Rias Gremory",
+
+	constructor(client: WAClient, handler: MessageHandler) {		super(client, handler, {
+
+			command: "rias",
+
 			description: "Displays the info",
+
 			category: "general",
-			usage: `${client.config.prefix}chitoge`,
-			baseXp: 200,
+
+			usage: `${client.config.prefix}zerotwo`,
+
+		        dm: true,
+
+                        aliases: ['rias','?1','300','menu1']
+
 		});
+
 	}
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
-		const chitoge =
-			"https://c.tenor.com/j_oNsBf9Iw8AAAPo/high-school-dxd-rias-gremory.mp4 ";
+
+		const well = 
+
+			"https://c.tenor.com/j_oNsBf9Iw8AAAPo/high-school-dxd-rias-gremory.mp4";
+
 		return void this.client.sendMessage(
+
 			M.from,
-			{ url: chitoge },
+
+			{ url: well },
+
 			MessageType.video,
+
 			{
+
 				quoted: M.WAMessage,
+
 				mimetype: Mimetype.gif,
-				caption: `🌟 *Rias Gremory* 🌟\n\n🍀 *Description: A WhatsApp Bot With Rich Anime Features.*\n\n🌐 *URL: https://github.com/VaseemStarkJr3000/Rias-Gremory* \n\n 📒 *Guide: https://github.com/ShineiIchijo/Chitoge-Guides* \n`,
+
+				caption: `━━❰•𝙉𝙨𝙛𝙬•❱━━
+
+    
+
+🌈 ${this.client.config.prefix}ᴀɴᴀʟ 
+
+🌈 ${this.client.config.prefix}ʙʟᴏᴡᴊᴏʙ 
+
+🌈 ${this.client.config.prefix}ʜꜱᴇᴀʀᴄʜ 
+
+🌈 ${this.client.config.prefix}ᴍᴀɪᴅ 
+
+🌈 ${this.client.config.prefix}ᴍᴀꜱᴛᴜʀʙᴀᴛɪᴏɴ 
+
+🌈 ${this.client.config.prefix}ɴʜᴇɴᴛᴀɪ
+
+🌈 ${this.client.config.prefix}ɴꜱꜰᴡᴋɪᴛꜱᴜɴᴇ 
+
+🌈 ${this.client.config.prefix}ɴꜱꜰᴡʟᴏʟɪ 
+
+🌈 ${this.client.config.prefix}ɴꜱꜰᴡᴘᴀᴘᴇʀ 
+
+🌈 ${this.client.config.prefix}ɴꜱꜰᴡɴᴇᴋᴏ 
+
+🌈 ${this.client.config.prefix}ɴꜱꜰᴡᴡᴀɪꜰᴜ 
+
+🌈 ${this.client.config.prefix}ᴘᴜꜱꜱʏ 
+
+🌈 ${this.client.config.prefix}ʀʜᴇɴᴛᴀɪ 
+
+🌈 ${this.client.config.prefix}ᴛʜɪɢʜꜱ
+
+•━━━ ✽ • ✽ ━━━•`,
+
 			}
+
 		);
+
 	};
+
 }
