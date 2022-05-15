376f404176a2ed4e911bdc03f7e6d230e248790b
Issei <74949779+VaseemStarkJr3000@users.noreply.github.com>
2022-05-15 22:42:50 +0530

    Update Enable.ts

--- src/commands/Dev/Enable.ts
+++ src/commands/Dev/Enable.ts
@@ -18,12 +18,12 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
         const key = joined.toLowerCase().trim()
-        if (!key) return void (await M.reply(`Provide the command you want to enable`))
+        if (!key) return void (await M.reply(`Provide the command you want to enable Baka`))
         const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
         if (!command) return void (await M.reply(`No command found`))
         if (!(await this.client.DB.disabledcommands.findOne({ command: command.config.command })))
-            return void M.reply(`${this.client.util.capitalize(command.config.command)} is already enabled`)
+            return void M.reply(`${this.client.util.capitalize(command.config.command)} is already enabled Baka`)
         await this.client.DB.disabledcommands.deleteOne({ command: command.config.command })
-        await M.reply(`*${this.client.util.capitalize(command.config.command)}* is now Enabled`)
+        await M.reply(`*${this.client.util.capitalize(command.config.command)}* is now Enabled Enjoy yourselfe Baka`)
     }
 }
