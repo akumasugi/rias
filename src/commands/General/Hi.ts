a5347b4b0cc2c386dd791de8d1dc546b8ecd8e15
Issei <74949779+VaseemStarkJr3000@users.noreply.github.com>
2022-05-16 00:14:53 +0530

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -1,34 +1,57 @@
-/** @format */
-
 import { MessageType, Mimetype } from "@adiwajshing/baileys";
+
 import MessageHandler from "../../Handlers/MessageHandler";
+
 import BaseCommand from "../../lib/BaseCommand";
+
 import WAClient from "../../lib/WAClient";
+
 import { ISimplifiedMessage } from "../../typings";
 
 export default class Command extends BaseCommand {
-	constructor(client: WAClient, handler: MessageHandler) {
-		super(client, handler, {
+
+	constructor(client: WAClient, handler: MessageHandler) {		super(client, handler, {
+
 			command: "hi",
+
 			description: "Generally used to check if bot is Up",
+
 			category: "general",
+
 			usage: `${client.config.prefix}hi`,
+
 			baseXp: 10,
+
 		});
+
 	}
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
-		const chitoge =
-			"https://media.tenor.com/videos/8a6408675b1f806290aaac4b939c5a98/mp4";
+
+		const zerotwo =
+
+			"https://c.tenor.com/tkjZFMOLnmMAAAPo/anime-rias-gremory.mp4";
+
 		return void this.client.sendMessage(
+
 			M.from,
-			{ url: chitoge },
+
+			{ url: zerotwo },
+
 			MessageType.video,
+
 			{
+
 				quoted: M.WAMessage,
+
 				mimetype: Mimetype.gif,
-				caption: `I don't have time to have a conversation with someone like you Baka . Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
-			}
+
+				caption: `*_yamete kudasai 🍒_*`,
+
+		    }
+
 		);
+
 	};
+
 }
